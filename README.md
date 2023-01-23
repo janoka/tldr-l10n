e4907fecd953a7c3216a69610994efc2b3e169b5
Janos Kuszing <github.com@office.netstudio.hu>
2023-01-23 09:30:53 +0100

    Initial commit for the project.

---
+++ README.md
@@ -0,0 +1,59 @@
+# Automated translation for TLDR pages
+
+## What is this?
+
+This is a project to automatically translate TLDR pages into other languages. The goal is to make it easy for people to contribute translations, and to make it easy for people to find translations.
+
+## How does it work?
+
+The TLDR pages are stored in a git repository. Each page is stored in a separate file, and each file is named after the command it describes. For example, the page for the `git` command is stored in the file `git.md`.
+
+Please see more on the [TLDR project](https://github.com/tldr-pages/tldr).
+
+## Prerequisites
+
+- Python 3.9 or higher
+- Python virtual environment module
+- Deepl API key, see [here](https://www.deepl.com/pro#developer)
+- Git and GitHub account
+
+## How can I install it?
+
+```shell
+git clone https://github.com/janoka/tldr-t9n.git
+cd tldr-t9n
+
+# Initialize the python virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
+python3 -m pip install -r requirements.txt
+```
+
+Make a fork of [TLDR Pages](https://github.com/tldr-pages/tldr) at GitHub to your account there and clone it to your computer.
+
+E.g.:
+
+```shell
+# Following you forked the TLDR pages repository to your account:
+git clone https://github.com/janoka/tldr.git
+```
+
+Go back to the project folder and make a copy of the [`env.example`](env.example) file and name it `.env`. Then edit the `.env` file and set the `TLDR_REPO` variable to the path of the TLDR repository on your computer.
+
+```shell
+python3 main.py
+```
+
+## Credits
+
+- [TLDR Pages](https://github.com/janoka/tldr)
+- [Deepl API for automated translation](https://www.deepl.com/pro#developer)
+- [Python](https://www.python.org/)
+  - [Python PIP](https://pypi.org/project/pip/)
+  - [Python Virtual Environment](https://docs.python.org/3/library/venv.html)
+- [Git](https://git-scm.com/)
+- [GitHub](https://github.com)
---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+import deepl as deepl
+import markdown
+from markdownify import markdownify
+from mdformat import text as mdformat
+from dotenv import load_dotenv
+
+
+def parse_source_folder():
+    """Create non-existing target folder, according to the source folder"""
+    # Import environment variables from .env file
+    load_dotenv()
+
+    # Initialize sources list
+    sources = []
+
+    # Get source folder
+    source_folder = f"{os.getenv('TLDR_ROOT')}/pages"
+
+    # Get subfolders in source folder
+    sub_folders = [folder for folder in os.listdir(source_folder) if os.path.isdir(
+        f"{source_folder}/{folder}")]
+
+    # Get all files in source_folder and its subfolders
+    for folder in sub_folders:
+        files = [file for file in os.listdir(
+            f"{source_folder}/{folder}") if os.path.isfile(f"{source_folder}/{folder}/{file}")]
+        for file in files:
+            sources.append(f"{source_folder}/{folder}/{file}")
+
+    return sources
+
+
+def readfile(file: str):
+    """Read the markdown file"""
+    markdown_file = open(file, "r")
+    markdown_text = markdown_file.read()
+    markdown_file.close()
+    return markdown_text
+
+
+def savefile(target: str, text: str):
+    """Save the file"""
+    with open(target, "w") as file:
+        file.write(text)
+
+
+def target(source: str):
+    # Split the path to filename and sub folder name.
+    # Example: /Users/username/tldr/pages/osx/gdf.md -> osx, gdf.md
+    folder, md = source.split("/")[-2:]
+
+    # Return the target folder string
+    # Example: /Users/username/tldr/pages.hu/osx/gdf.md
+    target_file = f"{os.getenv('TLDR_ROOT')}/pages.{os.getenv('TLDR_LANG')}/{folder}/{md}"
+
+    # Create the target folder if it doesn't exist
+    if not os.path.exists(os.path.dirname(target_file)):
+        os.makedirs(os.path.dirname(target_file))
+
+    return target_file
+
+
+if __name__ == '__main__':
+    sources = parse_source_folder()
+
+    for source in sources[0:1400]:
+        md = readfile(source)
+        target_file = target(source)
+
+        # Do the following if the file is not exist
+        if not os.path.exists(target_file):
+            # Convert markdown to html
+            html = markdown.markdown(md)
+            # Add translate="no" attribute to all <h1>, <pre>, and <code> tags
+            html = html.replace('<h1>', '<h1 translate="no">')
+            html = html.replace('<pre>', '<pre translate="no">')
+            html = html.replace('<code>', '<code translate="no">')
+
+            translator = deepl.Translator(os.getenv('DEEPL_API_KEY'))
+            html = str(translator.translate_text(text=html,
+                                                 tag_handling='html',
+                                                 source_lang='en',
+                                                 target_lang=os.getenv('TLDR_LANG'),
+                                                 ignore_tags=['h1', 'pre', 'code']))
+            # Convert html to markdown
+            md_export = mdformat(markdownify(
+                html, heading_style='atx', bullets='-'))
+            savefile(target_file, md_export)
